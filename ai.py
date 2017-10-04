b2cdabb434af335dc5515058a0f75f5d97614aef
Hugo Genesse <hugo@pnfsoftware.com>
2017-10-04 17:05:28 -0400

    Deserialize_map implemented

--- ai.py
+++ ai.py
@@ -1,10 +1,25 @@
 from flask import Flask
+from structs import Tile
 app = Flask(__name__)
+
 
 def deserialize_map(serialized_map):
     """
     Fonction utilitaire pour comprendre la map
     """
+    serialized_map = serialized_map[1:]
+    rows = serialized_map.split('[')
+    column = rows[0].split('{')
+    deserialized_map = {}
+
+    for i in range(len(rows)):
+        column = rows[i + 1].split('{')
+
+        for j in range(len(column)):
+            infos = column[j + 1].split(',')
+            deserialized_map[i] = Tile(int(infos[0]), int(infos[1]), int(infos[2].[0, infos[2].find('}')]))
+
+    return deserialized_map
 
 def bot():
     """
